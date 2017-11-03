0c1df030585ce3f15cc4e38082d0529201274ca7
Daniel O'Malley <omalled@lanl.gov>
2017-11-03 17:07:59 -0600

    remove old code that is no longer needed

--- src/CanDecomp.jl
+++ src/CanDecomp.jl
@@ -46,16 +46,6 @@ totensor(matrices...) = totensor(StaticArrays.SVector(matrices...), tensordims(m
 	return code
 end
 
-function estimatethirdmatrixcolumn(i_3, tensorslice_i_3, matrices::StaticArrays.SVector, dims; regularization=1e0, kwargs...)
-	m = JuMP.Model(solver=Ipopt.IpoptSolver(; kwargs...))
-	facrank = size(matrices[1], 1)
-	@JuMP.variable(m, Ucol_i_3[j=1:facrank], start=matrices[3][j])
-	@JuMP.constraint(m, Ucol_i_3 .>= 0)
-	@JuMP.objective(m, Min, sum((tensorslice_i_3[i_1, i_2] - sum(matrices[1][l, i_1] * matrices[2][l, i_2] * Ucol_i_3[l] for l=1:facrank))^2 for i_1 = 1:dims[1] for i_2 = 1:dims[2]) + regularization * sum(Ucol_i_3[l]^2 for l=1:facrank))
-	JuMP.solve(m)
-	return JuMP.getvalue(Ucol_i_3)
-end
-
 @generated function estimatecolumnoflastmatrix(i_n, tensorslice_i_n, matrices::StaticArrays.SVector{N, T}, dims; regularization=1e0, kwargs...) where {N, T}
 	q = macroexpand(:(@ngenerator $(N - 1) (((@nref $(N - 1) tensorslice_i_n i) - sum((@nprod $(N - 1) j->matrices[j][l, i_j]) * Ucol_i_n[l] for l = 1:facrank))^2) j->i_j = 1:dims[j]))
 	code = quote
